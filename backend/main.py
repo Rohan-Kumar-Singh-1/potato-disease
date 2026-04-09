476c37bbd5dfa2da89598d10fce7d0351f0f6e87
Rohan-Kumar-Singh-1 <rohan300504@gmail.com>
2026-04-09 11:44:32 +0000

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -116,5 +116,6 @@ async def predict(file: UploadFile = File(...)):
 }
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
